c77f6922ecfa822d7fda099626a67b90f28f25ea
= <=>
2022-09-28 14:26:39 +0200

    Secondo Commit

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import serial
 
 microcontroller = serial.Serial("/dev/cu.usbmodem14101", 9600);
 
-from telethon import TelegramClient, events, sync
-
-import requests
-
-
 def read_from_terminal():
     # lettura del comando da input
     comando = "spegni"
@@ -20,29 +15,10 @@ def read_from_terminal():
     microcontroller.flush()
     microcontroller.close()
 
-def read_from_telegram():
-
-    TOKEN = "5544630619:AAE997m6LfflMbyAPFOnR6AbIctfDQNMA44"
-    url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
-    # lettura comando da canale telegram
-    #api_id =
-    #api_hash =
-    #client = TelegramClient('arduino', api_id, api_hash)
-
-    #@client.on(events.NewMessage(chats='channel_name'))
-    #async def my_event_handler(event):
-    #    print(event.raw_text)
-
-    #client.start()
-    #client.run_until_disconnected()
-
-    print(requests.get(url).json())
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     read_from_terminal()
-
 
     print("fine")
 
